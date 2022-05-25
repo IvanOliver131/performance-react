70e881fee27ea71cf11d84631b3a9ec3aee22668
Ivan Lucas de Oliveira Ribeiro <ivanoliver131@gmail.com>
2022-05-24 21:09:32 -0300

    devCommit

--- components/ProductItem/index.tsx
+++ components/ProductItem/index.tsx
@@ -1,3 +1,5 @@
+import { memo } from 'react';
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,10 +8,23 @@ interface ProductItemProps {
   }
 }
 
-export function ProductItem({ product }: ProductItemProps) {
+function ProductItemComponent({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
     </div>
   );
 }
+
+// O memo impede que caso nada mude nao precise renderizar de novo
+// eslint-disable-next-line react/display-name
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+  return Object.is(prevProps.product, nextProps.product)
+});
+
+/** Quando usar o memo
+ * 1. Pure Functional Components
+ * 2. Renders too often
+ * 3. Re-renders with same props
+ * 4. Medium to big size 
+ */
